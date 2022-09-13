24c17dae247ae9d290c51e85325614da3da7a06c
Shubham Kumar <shubhamk9097@gmail.com>
2022-09-13 19:55:15 +0530

    que2

---
+++ question2.js
@@ -0,0 +1,11 @@
+// Problem 2 : Check whether a string is palindrome or not.
+let str="naman"
+let x=""
+for(let i=str.length-1; i>=0; i--){
+    x=x+str[i]
+}
+if(str==x){
+    console.log("No")
+}else{
+    console.log("Yes")
+}

7cafc1e0ce642281e48f4f5758047172a36130c4
Khushi Gupta <khushigupta.of@gmail.com>
2022-09-13 18:18:18 +0530

    git 2

---
+++ login.js
@@ -0,0 +1,11 @@
+let reg_email = "Khushigupta.of@gmail.com"
+let reg_pass = "Khushigupta018"
+
+let entered_email = "khushigupta.of@gmail.com"
+let eneterd_pass = "khushi"
+
+if(reg_email==entered_email && reg_pass===entered_pass){
+     console.log("Login successful");
+}else{
+    console.log("Something Wrong");
+}
